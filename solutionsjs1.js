ca52bb9875c5c7aee9b66ebbcdc93d35d4b35945
Hackbright Student <info@hackbrightacademy.com>
2015-01-27 17:52:12 -0800

    working fibonacci code

--- solutionsjs1.js
+++ solutionsjs1.js
@@ -16,10 +16,10 @@ function summing(x,y){
 function fibonacciSet(max){
     if (max > 1){
         var fibArray = [1];
-        var currentFib = 5;
+        var currentFib = 1;
         while (currentFib < max){
             fibArray.push(currentFib);
-            var lastNum = fibArray.slice(-1); 
+            var lastNum = fibArray.slice(-1);
             var secLastNum = fibArray.slice(-2, -1);
             // console.log(lastNum, secLastNum);
             currentFib = summing(lastNum, secLastNum);
@@ -35,5 +35,26 @@ function fibonacciSet(max){
     }
 }
 
+function isEven(resultArray){
+    var evenNum = [];
+    for (var i = 0; i < resultArray.length; i++){
+        if (resultArray[i] % 2 === 0){
+            evenNum.push(resultArray[i]);
+        }
+    }
+    return evenNum;
+}
 
-fibonacciSet(40);
+function sumArray(evenArray){
+    var total = 0;
+    for (var i = 0; i < evenArray.length; i++){
+        total = total + evenArray[i];
+    }
+    return total;
+}
+
+sumArray(isEven(fibonacciSet(4000000)));
+
+// return a list of the even numbers of the fib function
+    //make a function that loops through returned fib list and makes a list of even numbers
+// sum them together
