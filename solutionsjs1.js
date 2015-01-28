cd5117d85c1405d628b83d520fc4eec6fcebd5b6
Hackbright Student <info@hackbrightacademy.com>
2015-01-28 13:08:16 -0800

    Got question 3 working

--- solutionsjs1.js
+++ solutionsjs1.js
@@ -1,13 +1,14 @@
-// //Question 1
+//QUESTION 1
 
-// var sum = 0;
-// for(var i = 0; i < 1000; i++){
-//     if(i % 3 === 0 || i % 5 === 0){
-//          sum += i}
-// }
-// console.log(sum)
+var sum = 0;
+for(var i = 0; i < 1000; i++){
+    if(i % 3 === 0 || i % 5 === 0){
+         sum += i}
+}
+console.log(sum)
+// Kristen says it's beautiful. <3 <3
 
-//Question 2
+//QUESTION 2
 
 function summing(x,y){
     return Number(x) + Number(y);
@@ -58,3 +59,30 @@ sumArray(isEven(fibonacciSet(4000000)));
 // return a list of the even numbers of the fib function
     //make a function that loops through returned fib list and makes a list of even numbers
 // sum them together
+
+// QUESTION 3
+var votesToGoEatCake = [true, true, true, true];
+var hackbrightStudents = ["katie", "amy", "jenny", "katie", "kelley", "katie", "amy"];
+var classroomIds = ["47","12", "19", "22", "26", "99", "30", "50", "324", "003", "44",
+"33", "346", "354", "44", "235", "45", "34", "44", "590", "09", "099", "0", "1", "3","33", "999", "9"];
+var randomJunkIFound = ["katie", "true", true, 19, "gargoyles", "!", 2 + 3, "2 + 3", 19, "19", 19 === "19", 6, false, false];
+
+    //look in dupe array for me, if i'm not there already AND
+        //if I am in items in original array index-after-me to end-of-array
+            //put me in the dupe array
+
+function theDupes(myArray){
+    dupeArray = [];
+    for (i = 0; i < myArray.length; i++){
+        var afterMe = myArray.slice(i,myArray.length);
+        if (dupeArray.indexOf(myArray[i]) === -1 && afterMe.indexOf(myArray[i]) !== -1){
+            dupeArray.push(myArray[i]);
+        }
+    } console.log(dupeArray);
+}
+
+theDupes(votesToGoEatCake);
+theDupes(hackbrightStudents);
+theDupes(classroomIds);
+theDupes(randomJunkIFound);
+///
